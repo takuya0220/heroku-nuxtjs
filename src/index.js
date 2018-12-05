cf74c964f365c25ab4ad3f93df1796754e173e29
TakuyaMaeda <takuya3753@gmail.com>
2018-12-05 22:50:29 +0900

    node.jsが動く環境を作成

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { Nuxt, Builder } = require("nuxt");
+
+const app = express();
+
+let config = require("../nuxt.config.js");
+config.dev = !(process.env.NODE_ENV === "production");
+const nuxt = new Nuxt(config);
+
+if (config.dev) {
+  const builder = new Builder(nuxt);
+  builder.build();
+}
+
+app.use(nuxt.render);
+
+module.exports = app;
