8c3497896e66fb36fcf22a430fff57af3ca9f856
TakuyaMaeda <takuya3753@gmail.com>
2018-12-06 00:30:35 +0900

    vuetifyが動作するよう設定

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container>
+        <nuxt/>
+      </v-container>
+    </v-content>
+    <v-footer>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+<script>
+import Vue from "vue";
+
+export default class extends Vue {}
+</script>
